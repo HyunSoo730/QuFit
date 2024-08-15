af65e48b4de94b8c4b63c51f0089ec3b5b090718
hyunsoo730 <whgustn730@gmail.com>
2024-08-15 11:30:55 +0900

    Feat: 로그 찍기

--- front-end/src/components/video/ParticipantVideo.tsx
+++ front-end/src/components/video/ParticipantVideo.tsx
@@ -17,10 +17,18 @@ const ParticipantVideo = ({ roomMax, gender, status, participants }: Participant
         <div className="flex justify-center w-full gap-1 ">
             {participants.map((participant) => {
                 if (participant.gender === gender) {
+                    const videoTrack = participant.info!.videoTrackPublications.values().next().value?.videoTrack || undefined;
+                    
+                    // 비디오 트랙이 없을 경우 렌더링을 하지 않음
+                    if (!videoTrack) {
+                        console.warn("ParticipantVideo: 비디오 트랙이 설정되지 않았습니다 - 참가자 이름:", participant.nickname);
+                        return null; 
+                    }
+
                     console.log("ParticipantVideo: 참가자 렌더링 - 이름:", participant.nickname, "ID:", participant.id);
                     console.log("ParticipantVideo: FaceLandmarker 준비 상태:", participant.faceLandmarkerReady);
                     console.log("ParticipantVideo: FaceLandmarker 객체:", participant.faceLandmarker);
-                    console.log("ParticipantVideo: 비디오 트랙:", participant.info!.videoTrackPublications.values().next().value?.videoTrack);
+                    console.log("ParticipantVideo: 비디오 트랙:", videoTrack);
 
                     numPeople++;
                     return (
@@ -28,9 +36,7 @@ const ParticipantVideo = ({ roomMax, gender, status, participants }: Participant
                             roomMax={roomMax}
                             key={participant.id} // participant.nickname에서 participant.id로 변경하여 고유성을 보장
                             id={participant.id}
-                            track={
-                                participant.info!.videoTrackPublications.values().next().value?.videoTrack || undefined
-                            }
+                            track={videoTrack}
                             isManager={participant.id === hostId}
                             participateName={participant.nickname!}
                             faceLandmarkerReady={participant.faceLandmarkerReady}
